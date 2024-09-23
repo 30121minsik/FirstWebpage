21e07fa148db4701d3bfd8fd6a7d96ef89c1e5bd
30121minsik <22167@dongdaemun.ms.kr>
2024-09-23 15:38:23 +0900

    Update 02-text.py

--- pages/02-text.py
+++ pages/02-text.py
@@ -12,3 +12,19 @@ st.header('헤던는 섹션의 제목 :sparkles:')
 
 
 st.subheader('subheader는 섹션의 부제목')
+
+
+
+
+st.caption('캡션은 짧은 설명을 추가하는 것!')
+
+
+sample_code = '''
+def fimction():
+    print("life is too short,you need the python")
+'''
+st.code(smaple_code, language="python"
+
+
+st.text('일반적인 텍스트를 입력해 보았습니다.')
+        
