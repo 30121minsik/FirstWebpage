600922f86ce3d8038765b453e1160fdd6e2d4b01
30121minsik <22167@dongdaemun.ms.kr>
2024-09-23 15:56:38 +0900

    Update 02-text.py

--- pages/02-text.py
+++ pages/02-text.py
@@ -31,8 +31,8 @@ st.text('이 커뮤니티는 인스타그램을 뛰어넘을 것입니다.')
 
 st.markdown('streamlit은 **마크다운 문법을 지원 **합니다.')
 
-st..markdown("텍스트의 색상을 green[초록색]으로, 그리고 **blue[파란색]** 볼트체로 설정할 수 있습니다.")
-st.markdown("green[$/sqrt{1+1}=2$'] 와 같이 latex 문법의 수식 표현도 가능합니다 :pencil")
+st..markdown("텍스트의 색상을 :green[초록색]으로, 그리고 **:blue[파란색]** 볼트체로 설정할 수 있습니다.")
+st.markdown(":green[$/sqrt{1+1}=2$'] 와 같이 latex 문법의 수식 표현도 가능합니다 :pencil")
 
 
 st.latex(r' \sqrt{1+1}=2')
