6902f4e822d7fe34d4dee69a835015d48dfe7ee6
30121minsik <22167@dongdaemun.ms.kr>
2024-09-30 15:43:00 +0900

    Update 03-basic.ui.py

--- pages/03-basic.ui.py
+++ pages/03-basic.ui.py
@@ -4,10 +4,11 @@ from datetime import datetime as dt
 import datetime
 
 
+
 button = st.button ('버튼을 눌러보세요')
 if button:
   st.write (':blue[버튼]이 눌렸습니다. :sparkles:')
-dataframe = pd.Dataframe({
+dataframe = pd.DataFrame({
   'first column ' : ['kor','eng','math','science'],
   'second column': [10, 20, 30, 40]
 })
