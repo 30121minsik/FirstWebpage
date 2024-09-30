ba9121b986684753a40f4bbe583ed9ad1de55c57
30121minsik <22167@dongdaemun.ms.kr>
2024-09-30 15:53:06 +0900

    Update 03-basic.ui.py

--- pages/03-basic.ui.py
+++ pages/03-basic.ui.py
@@ -21,7 +21,7 @@ st.download_button(
 agree =st.checkbox('동의 하십니까?')
 if agree:
   st.write('동의 해주셔서 감사합니다 :100:')
-  mbti =dt.radio(
+  mbti =st.radio(
   '당신의 MBTI는 무엇입니까?',
   ('ISTJ','ENFP','선택지 없음'))
   if mbti == 'ISTJ':
