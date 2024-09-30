725a2d3788b663e9c6c71caac160ef4087e5eb31
30121minsik <22167@dongdaemun.ms.kr>
2024-09-30 15:33:19 +0900

    Update 03-basic.ui.py

--- pages/03-basic.ui.py
+++ pages/03-basic.ui.py
@@ -9,7 +9,7 @@ if button:
   st.write (':blue[버튼]이 눌렸습니다. :sparkles:')
 dataframe = pd.dataframe({
   'first column ' : ['kor',eng','math','science'],
-    'second column': [10, 20, 30, 40]
+  'second column': [10, 20, 30, 40]
                      })
 st.download_button(
   label='csv로 성적표 다운로드',
