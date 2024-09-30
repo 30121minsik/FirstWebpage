c5079a3ac9fa5aea6a9fc1bb350b204aafae1ff7
30121minsik <22167@dongdaemun.ms.kr>
2024-09-30 15:31:55 +0900

    Update 03-basic.ui.py

--- pages/03-basic.ui.py
+++ pages/03-basic.ui.py
@@ -7,3 +7,13 @@ import datetime
 button = st.button ('버튼을 눌러보세요')
 if button:
   st.write (':blue[버튼]이 눌렸습니다. :sparkles:')
+dataframe = pd.dataframe({
+  'first column ' : ['kor',eng','math','science'],
+    'second column': [10 20, 30, 40]
+                     })
+st.download_button(
+  label='csv로 성적표 다운로드',
+  data=dataframe.to_csv(), 
+  file_name+'sample.csv',
+  mime='text/csv'
+)                 
