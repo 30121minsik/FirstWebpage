19381dacf7f9f4039c95374d2af08315db40d0d9
30121minsik <22167@dongdaemun.ms.kr>
2024-10-07 15:33:19 +0900

    Update 04-lotto.py

--- pages/04-lotto.py
+++ pages/04-lotto.py
@@ -6,8 +6,9 @@ st.title(':sparkled:로또 생성기:sparkles:')
 
 
 
-def gemerate_lotto():
+def generate_lotto():
   lotto = set()
+  
   while len(lotto) < 6:
     number = random.randint(1, 46)
     lotto.add(number)
