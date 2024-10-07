777225a8f3b6ac0fd8cd821d31f157338de4390f
30121minsik <22167@dongdaemun.ms.kr>
2024-10-07 15:39:59 +0900

    Update 04-lotto.py

--- pages/04-lotto.py
+++ pages/04-lotto.py
@@ -10,11 +10,11 @@ def generate_lotto():
   lotto = set()
   
   while len(lotto) < 6:
-    number = random.randint(1, 46)
-    lotto.add(number)
-    lotto =list(lotto)
-    lotto.sort()
-    return lotto
+      number = random.randint(1, 46)
+      lotto.add(number)
+      lotto = list(lotto)
+      lotto.sort()
+      return lotto
 
 button = st.button('로또를 생성해 주세요!')
 if button:
