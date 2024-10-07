4daf0c05112971922662528d061d498c8690e977
30121minsik <22167@dongdaemun.ms.kr>
2024-10-07 15:28:21 +0900

    Update 04-lotto.py

--- pages/04-lotto.py
+++ pages/04-lotto.py
@@ -18,4 +18,4 @@ def gemerate_lotto():
     if button:
        for i in range(1,6):
           st.subheader(f'{i}. 행운의 번호: :green[{generate_lotto()}]')
-      st.write(f"생선된 시각:{datetime.datetime.now().strftime('%y-%m-%d %H:%M")}")
+      st.write(f"생선된 시각: {datetime.datetime.now().strftime('%y-%m-%d %H:%M")}")
